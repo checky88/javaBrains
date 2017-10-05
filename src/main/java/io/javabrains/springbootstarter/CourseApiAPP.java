3873bc4eacf9888dd74dd6a5b2d0cd0f23071e12
checky88 <James1988>
2017-10-05 14:07:57 +0100

    First commit

---
+++ src/main/java/io/javabrains/springbootstarter/CourseApiAPP.java
@@ -0,0 +1,17 @@
+package io.javabrains.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiAPP {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CourseApiAPP.class, args);
+		
+		
+	}
+
+}
+	
